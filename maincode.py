3cf1d1d8378b099af32e8a5859c8947bca39c2ba
Dorine0702 <dorine.lachat@icloud.com>
2024-12-01 15:15:39 +0100

    modules imported

--- maincode.py
+++ maincode.py
@@ -1,9 +1,12 @@
+#! C:\Users\dorin\OneDrive\Documents\Université\HSG\SA2024\XCampHSG\Group project\PythonprojectHSG\venvtradinggame\Scripts\python.exe
+
 # 1) --- FETCHING LIVE STOCK PRICES ---
 
 # the module yfinance is necessary to fetch live stock prices from yahoo finance API
 import yfinance as yf 
 import numpy as np
 import pandas as pd
+
 
 # define the stocks traded in the game
 stocks={"Nvidia":"NVDA","Meta":"META","Microsoft":"MSFT","Alphabet":"GOOGL","AMD":"AMD"}
@@ -12,15 +15,19 @@ print(f"the stocks being traded are:{stocks.keys()}")
 livestock_prices={}
 stockprices_history=pd.DataFrame(columns=["name","price"]) #store all the stock prices (inclusive forecasts) in a dataframe as an history
 
+
 def get_live_data():
     """ Fetch live stock prices from Yahoo finance API and store the prices in a dictionary""" 
+    #global variables modified whithin the function
+    global livestock_prices
+    global stockprices_history
+
     for names,symbol in stocks.items():
         try:
             stock=yf.Ticker(symbol) 
-
             #stock.history is a dataframe. The following line captures the item on the last row of the column "close"
             #iloc[0] selects the first item of the series obtained by stock.history(period="1d").tail(1)["close"]
-            price=stock.history(period="1d").tail(1)["close"].iloc[0]
+            price=stock.history(period="5d").tail(1)["Close"].iloc[0]
             #corresponding prices added to the prices dictionary
             livestock_prices[names]=price
         except Exception as e:
@@ -28,7 +35,8 @@ def get_live_data():
 
     #update the history with the live prices
     for names, prices in livestock_prices.items():
-         price_history=stockprices_history.append({"name":names,"price":prices},ignore_index=True)
+        stockprices_history.loc[len(stockprices_history)]={"name":names,"price":prices} #add a new row to the dataframe
+       
 
     return livestock_prices
 
@@ -56,7 +64,7 @@ def simulate_stock_prices():
 
     #update the stock prices history
     for names, prices in list_lastprices.items():
-         stockprices_history.loc[len(stockprices_history)]={"name":names,"price":prices} #add a new row to the dataframe
+         stockprices_history.loc[len(stockprices_history)]={"name":names,"price":prices} 
     return simulated_prices   
 
 
